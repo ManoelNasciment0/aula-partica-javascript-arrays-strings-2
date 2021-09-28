07a2e31552d346a72b68a79ddba84c69d501eb6e
ManoelNasciment0 <manoelnascimentofernandes@gmail.com>
2021-09-28 13:35:57 -0300

    main

---
+++ atividade1.js
@@ -0,0 +1,9 @@
+var temperatura = [54, 78, 56, 98, 0, 12, 11, 37];
+console.log((54 - 32) / 1.8 + " convertidas de Fahrenheit para Celsius.");
+console.log((78 - 32) / 1.8 + " convertidas de Fahrenheit para Celsius.");
+console.log((56 - 32) / 1.8 + " convertidas de Fahrenheit para Celsius.");
+console.log((98 - 32) / 1.8 + " convertidas de Fahrenheit para Celsius.");
+console.log((0 - 32) / 1.8 + " convertidas de Fahrenheit para Celsius.");
+console.log((12 - 32) / 1.8 + " convertidas de Fahrenheit para Celsius.");
+console.log((11 - 32) / 1.8 + " convertidas de Fahrenheit para Celsius.");
+console.log((37 - 32) / 1.8 + " convertidas de Fahrenheit para Celsius.");
